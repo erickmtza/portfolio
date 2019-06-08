13d75ca36dbd3377eabeafe1bc5886dabf4f81df
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-07 22:00:30 -0500

    Added horizontal scrolling to projects section

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
 
 $(function fades() {
-    $('.traits').fadeIn(2000, fadeout() );
+    $('.traits').fadeIn(2000, fadeout());
     heroFadeIn();
+    $('.portfolio').on('mousewheel DOMMouseScroll', function(event){
+
+        var delta = Math.max(-1, Math.min(1, (event.originalEvent.wheelDelta || -event.originalEvent.detail)));
+    
+        $(this).scrollLeft( $(this).scrollLeft() - ( delta * 40 ) );
+        event.preventDefault();
+    
+    });
 })
 
 function fadeout() {
